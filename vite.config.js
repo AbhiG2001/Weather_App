3be906d73efa815da357e31dfe8c5c8f7f888170
Abhishek <drabhi2126@gmail.com>
2025-06-03 18:17:05 +0530

    vite.config.js file updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,8 @@ export default defineConfig({
   server:{
     host:'0.0.0.0',
     port: 3002
+  },
+  define:{
+    'import.meta.env.VITE_WEATHER_API_KEY': JSON.stringify(import.meta.env.VITE_WEATHER_API_KEY)
   }
 })
